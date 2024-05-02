36593f5de52fa886374ed72e81fc21ecf45d3644
Dihad Hasan <130214955+Dihad@users.noreply.github.com>
2024-05-02 23:11:17 +0600

    Palindrome Number.py

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -1,5 +1,5 @@
-num = str(input('enter the number: ')) 
-reversed_num = str(num)[::-1]
+num = int(input('enter the number: ')) 
+reversed_num = num[::-1]
 if num == reversed_num:
     print('True')
 else:
